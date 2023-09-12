db015e49443ff1aa9d4ba3c40c4aa0d915f4a75b
Sinkan Suravita <89245013+subhasmita-credencesoft@users.noreply.github.com>
2023-09-12 17:41:24 +0530

    fix

--- src/app/views/landing/components/about-credencesoft/about-credencesoft.component.ts
+++ src/app/views/landing/components/about-credencesoft/about-credencesoft.component.ts
@@ -3,13 +3,38 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-about-credencesoft',
   templateUrl: './about-credencesoft.component.html',
-  styleUrls: ['./about-credencesoft.component.scss']
+  styleUrls: ['./about-credencesoft.component.scss'],
 })
 export class AboutCredencesoftComponent implements OnInit {
+  projectcount: number = 0;
+  accuratecount: number = 0;
+  clientcount: number = 0;
+  customerfeedback: number = 0;
+  projectcountstop: any = setInterval(() => {
+    this.projectcount++;
+    if (this.projectcount == 1000) {
+      clearInterval(this.projectcountstop);
+    }
+  }, 20);
+  accuratecountstop: any = setInterval(() => {
+    this.accuratecount++;
+    if (this.accuratecount == 5000) {
+      clearInterval(this.accuratecountstop);
+    }
+  }, 20);
+  clientcountstop: any = setInterval(() => {
+    this.clientcount++;
+    if (this.clientcount == 300) {
+      clearInterval(this.clientcountstop);
+    }
+  }, 10);
+  customerfeedbackstop: any = setInterval(() => {
+    this.customerfeedback++;
+    if (this.customerfeedback == 99) {
+      clearInterval(this.customerfeedbackstop);
+    }
+  }, 20);
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
