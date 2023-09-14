c9a2e752943f8ea0c441961a0d71fda28ce46e4b
Sinkan Suravita <89245013+subhasmita-credencesoft@users.noreply.github.com>
2023-09-14 17:06:57 +0530

    fix

--- src/app/views/landing/contact/contact.component.ts
+++ src/app/views/landing/contact/contact.component.ts
@@ -21,7 +21,7 @@ export class ContactComponent implements OnInit {
     this.meta.updateTag({property: 'og:title', content: "Get in Touch with BookOnePMS | BookonePMS"});
     this.meta.updateTag({property: 'og:description', content: "Have questions or need assistance? Contact the BookOnePMS team for expert support and solutions tailored to your property management needs."});
     this.meta.updateTag({property: 'og:text:description', content:"dfghjkl"});
-    this.meta.updateTag({property: 'og:image', content: 'https://bookonepms.com/assets/images/contactlogo.png'})
+    this.meta.updateTag({property: 'og:image:width', content: 'https://bookonepms.com/assets/images/contactlogo.png'})
 
 
 
