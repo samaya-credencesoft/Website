2072467457b08b30450cf7dda6753f71e65295dc
subhasmita-credencesoft <89245013+subhasmita-credencesoft@users.noreply.github.com>
2023-10-16 23:09:51 +0530

    fix

--- src/app/views/landing/book-one-connect/book-one-connect.component.ts
+++ src/app/views/landing/book-one-connect/book-one-connect.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Meta } from '@angular/platform-browser';
 import { Observable } from 'rxjs';
 import { BlogPostService } from 'src/services/blog-post.service';
 
@@ -10,13 +11,35 @@ import { BlogPostService } from 'src/services/blog-post.service';
 export class BookOneConnectComponent {
   backgroundColor: string;
   showCustomizer: boolean;
-  constructor(private contentfulService: BlogPostService,){
+  constructor(private contentfulService: BlogPostService,
+    private meta:Meta){
+    this.meta.updateTag({name: 'description', content: "Indulge in a remarkable stay at BookOne PMS where exceptional comfort meets affordability."});
 
+
+
+    // Facebook metadata
+    this.meta.updateTag({property: 'og:title', content: 'BookOne Connect | BookonePMS'});
+    this.meta.updateTag({property: 'og:site', content: '@AngularUniv'});
+    this.meta.updateTag({property: 'og:title', content: "BookOne Connect | BookonePMS"});
+    this.meta.updateTag({property: 'og:description', content: "BOOKONE Connect is a connectivity solution provided by BOOKONE that allows hotels to integrate their property management system (PMS) with various distribution channels and online travel agencies (OTAs)."});
+    this.meta.updateTag({property: 'og:text:description', content:"dfghjkl"});
+    this.meta.updateTag({property: 'og:image', content: 'https://bookonepms.com/assets/images/pricing.png'})
+
+
+
+    // Twitter metadata
+    this.meta.updateTag({name: 'twitter:card', content: 'subha'});
+    this.meta.updateTag({name: 'twitter:site', content: '@AngularUniv'});
+    this.meta.updateTag({name: 'twitter:title', content: "BookOne Connect | BookonePMS"});
+    this.meta.updateTag({name: 'twitter:description', content: "BOOKONE Connect is a connectivity solution provided by BOOKONE that allows hotels to integrate their property management system (PMS) with various distribution channels and online travel agencies (OTAs)."});
+    this.meta.updateTag({name: 'twitter:text:description', content:"dfghjkl"});
+    this.meta.updateTag({name: 'twitter:image', content: 'https://bookonepms.com/assets/images/pricing.png'});
   }
   blogPosts$ : Observable<any> | undefined;
 
   ngOnInit() {
     this.blogPosts$ = this.contentfulService.getAllEntries();
+    // console.log("xcvb"+JSON.stringify(this.blogPosts$ ))
   }
 
   changeBg(colorName) {
