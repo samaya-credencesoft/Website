7a226a09f9a170d07d0e0e8dbb03900a29d18940
Sinkan-Credencesoft <sinkansuravita@credencesoft.in>
2024-05-02 14:15:16 +0530

    menu and header changes

---
+++ src/app/views/landing/Header-Listingdetailsone/Header-Listingdetailsone.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-Header-Listingdetailsone',
+  templateUrl: './Header-Listingdetailsone.component.html',
+  styleUrls: ['./Header-Listingdetailsone.component.css']
+})
+export class HeaderListingdetailsoneComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
