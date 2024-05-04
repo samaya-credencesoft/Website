48f1e923b90a99372150130df0f0349b81184ce7
Sinkan-Credencesoft <sinkansuravita@credencesoft.in>
2024-05-04 17:54:05 +0530

    header nav menu

--- src/app/views/landing/Header-Listingdetailsone/Header-Listingdetailsone.component.ts
+++ src/app/views/landing/Header-Listingdetailsone/Header-Listingdetailsone.component.ts
@@ -6,14 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./Header-Listingdetailsone.component.css']
 })
 export class HeaderListingdetailsoneComponent implements OnInit {
-  showListItems: boolean = false;
+  showListItems: boolean = false; // For your existing toggle functionality
+  isOpened = true; // Initially open (for the vertical navbar)
+
   toggleListItems() {
     this.showListItems = !this.showListItems;
+  }
+
+  closeNavbar(): void {
+    this.isOpened = false; // Hides the vertical navbar
   }
 
   constructor() { }
 
   ngOnInit() {
   }
-
 }
