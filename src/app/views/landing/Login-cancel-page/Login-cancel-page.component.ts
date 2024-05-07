2fe4ac2f676e6914ccf74b9661776f594d972414
Sinkan-Credencesoft <sinkansuravita@credencesoft.in>
2024-05-07 12:30:59 +0530

    fixed

---
+++ src/app/views/landing/Login-cancel-page/Login-cancel-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-Login-cancel-page',
+  templateUrl: './Login-cancel-page.component.html',
+  styleUrls: ['./Login-cancel-page.component.css']
+})
+export class LoginCancelPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
