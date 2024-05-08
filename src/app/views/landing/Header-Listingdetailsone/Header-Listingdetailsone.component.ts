35b1a48840c7642ae6c3d9afb7207a7937af96f9
subhasmita-credencesoft <subhasmita.tripathy@credencesoft.co.nz>
2024-05-08 11:18:13 +0530

    header

--- src/app/views/landing/Header-Listingdetailsone/Header-Listingdetailsone.component.ts
+++ src/app/views/landing/Header-Listingdetailsone/Header-Listingdetailsone.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-Header-Listingdetailsone',
@@ -7,8 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderListingdetailsoneComponent implements OnInit {
   showListItems: boolean = false; // For your existing toggle functionality
-  isdone : boolean = true; 
-
+  isdone : boolean = false; 
+  @Input()
+  businessUser:any;
+  showListingDetails: boolean = false;
+  toggleListingDetails() {
+    this.showListingDetails = !this.showListingDetails;
+    this.isdone = true;
+  }
   toggleListItems() {
     this.showListItems = !this.showListItems;
   }
