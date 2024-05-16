e1b3923d7a93bd155bd87c91d6c896d7b2cb2b53
Sinkan-Credencesoft <sinkansuravita@credencesoft.in>
2024-05-16 22:59:32 +0530

    back app header

--- src/app/views/landing/Header-Listingdetailsone/Header-Listingdetailsone.component.ts
+++ src/app/views/landing/Header-Listingdetailsone/Header-Listingdetailsone.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-Header-Listingdetailsone',
@@ -40,11 +41,17 @@ export class HeaderListingdetailsoneComponent implements OnInit {
 
   }
 
+  goBack(): void {
+    this.location.back();
+  }
+
   closeNavbar(): void {
     this.isdone = false; // Hides the vertical navbar
   }
 
-  constructor(private router: Router) { }
+  constructor(private router: Router,
+    private location: Location,
+  ) { }
 
   ngOnInit() {
     this.website = this.businessUser.website;
