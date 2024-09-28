ba6f3efb5840c5f7187bb53c2a2bd8fdecc043b9
priyabrata-credencesoft <106064224+priyabrata-credencesoft@users.noreply.github.com>
2024-09-28 22:46:15 +0530

    enquiry api

--- src/app/views/landing/booking-complete/booking-complete.component.ts
+++ src/app/views/landing/booking-complete/booking-complete.component.ts
@@ -16,6 +16,7 @@ import { ActivatedRoute } from '@angular/router';
 import { EnquiryForm } from '../onboarding-roomdetails-form/onboarding-roomdetails-form.component';
 import { TokenStorage } from 'src/token.storage';
 import { HotelBookingService } from 'src/services/hotel-booking.service';
+import { EnquiryDto } from 'src/app/model/enquiry';
 
 @Component({
   selector: 'app-booking-complete',
@@ -341,6 +342,115 @@ this.hotelBookingService
           summary: 'The server is taking more than usual time,please try again after sometime.'
         });
       }, 25000); */
+      setTimeout(() => {
+        this.accommodationEnquiryBookingData();
+    }, 3000);
+  }
+
+
+  accommodationEnquiryBookingData(){
+    this.enquiryForm = new EnquiryDto();
+
+    if (this.token.getProperty().address != null && this.token.getProperty().address != undefined &&
+      this.token.getProperty().address.city != null && this.token.getProperty().address.city != undefined)
+    {
+      this.enquiryForm.country = this.token.getProperty().address.country;
+      this.enquiryForm.location = this.token.getProperty().address.city;
+      this.enquiryForm.alternativeLocation = this.token.getProperty().address.city;
+    }
+    this.payment.netReceivableAmount = this.booking.netAmount;
+    this.enquiryForm.min = this.booking.totalAmount;
+    this.enquiryForm.max = this.booking.totalAmount;
+    this.enquiryForm.totalAmount = this.booking.totalAmount;
+
+    this.enquiryForm.firstName = this.booking.firstName;
+    this.enquiryForm.lastName = this.booking.lastName;
+    this.enquiryForm.email = this.booking.email;
+    this.enquiryForm.phone = this.booking.mobile;
+    this.enquiryForm.checkOutDate = this.booking.toDate;
+    this.enquiryForm.checkInDate = this.booking.fromDate;
+    this.enquiryForm.noOfPerson = this.booking.noOfPersons;
+    this.enquiryForm.noOfExtraPerson=this.booking.noOfExtraPerson;
+    this.enquiryForm.roomId=this.booking.roomId;
+    this.enquiryForm.payableAmount=this.booking.netAmount;
+    this.enquiryForm.roomName=this.booking.roomName;
+    this.enquiryForm.extraPersonCharge=this.booking.extraPersonCharge;
+    this.enquiryForm.noOfExtraChild=this.booking.noOfExtraChild;
+    this.enquiryForm.externalSite="Website";
+    this.enquiryForm.source = "The Hotel Mate"
+    this.enquiryForm.beforeTaxAmount=this.booking.beforeTaxAmount;
+    this.enquiryForm.mobile=this.booking.mobile;
+    this.enquiryForm.roomType=this.booking.roomType;
+    this.enquiryForm.roomRatePlanName=this.booking.roomRatePlanName;
+    this.enquiryForm.createdDate = new Date();
+
+    this.enquiryForm.accountManager ='TheHotelMate Team';
+    this.enquiryForm.consultantPerson ='';
+    this.enquiryForm.noOfRooms = this.booking.noOfRooms;
+    this.enquiryForm.noOfChildren = this.booking.noOfChildren;
+    this.enquiryForm.accommodationType = this.token.getProperty().businessType;
+    this.enquiryForm.status = "Booked";
+    this.enquiryForm.specialNotes = this.booking.notes
+    this.enquiryForm.propertyId = 107;
+    this.enquiryForm.currency = this.token.getProperty().localCurrency;
+    this.enquiryForm.taxDetails = this.token.getProperty().taxDetails;
+    this.enquiryForm.planCode = this.booking.planCode;
+    this.enquiryForm.bookingReservationId = this.booking.propertyReservationNumber;
+
+    this.enquiryForm.bookingPropertyId = this.token.getProperty().id;
+    this.enquiryForm.propertyName = this.token.getProperty().name;
+
+    const TO_EMAIL = 'support@thehotelmate.com';
+    const TO_NAME = 'Support - The Hotel Mate';
+    const bccEmail = 'samaya.muduli@credencesoft.co.nz';
+    const bccEmail2 = 'info@bookonepms.com';
+    const bccName = 'Samaya';
+
+    this.enquiryForm.fromName =
+    this.enquiryForm.firstName + ' ' + this.enquiryForm.lastName;
+    this.enquiryForm.toName = TO_NAME;
+    this.enquiryForm.fromEmail = this.enquiryForm.email;
+    this.enquiryForm.toEmail = TO_EMAIL;
+    this.enquiryForm.bccEmail = bccEmail;
+    this.enquiryForm.bccName = bccEmail;
+    this.enquiryForm.bccEmailTo = bccEmail2;
+
+    if (
+      this.enquiryForm.dietaryRequirement === null ||
+      this.enquiryForm.dietaryRequirement === undefined
+    ) {
+      this.enquiryForm.dietaryRequirement = '';
+    }
+    if (
+      this.enquiryForm.accommodationType === null ||
+      this.enquiryForm.accommodationType === undefined
+    ) {
+      this.enquiryForm.accommodationType = '';
+    }
+    if (
+      this.enquiryForm.specialNotes === null ||
+      this.enquiryForm.specialNotes === undefined
+    ) {
+      this.enquiryForm.specialNotes = '';
+    }
+    if (
+      this.enquiryForm.alternativeLocation === null ||
+      this.enquiryForm.alternativeLocation === undefined
+    ) {
+      this.enquiryForm.alternativeLocation = '';
+    }
+    this.enquiryForm.foodOptions = '';
+    this.enquiryForm.organisationId = environment.parentOrganisationId;
+    this.paymentLoader = true;
+    this.enquiryForm.roomPrice = this.booking.roomPrice;
+    this.hotelBookingService.accommodationEnquiry(this.enquiryForm).subscribe((response) => {
+      this.enquiryForm = response.body;
+      this.paymentLoader = false;
+      this.paymentLoader = false;
+      this.isSuccess = true;
+      this.submitButtonDisable = true;
+      this.bookingConfirmed = true;
+    })
   }
 
   createEnquiry() {
