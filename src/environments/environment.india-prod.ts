6477d44b2ac51ce73d97256c10e416d788368a02
subhasmita-credencesoft <89245013+subhasmita-credencesoft@users.noreply.github.com>
2024-10-17 19:07:56 +0530

    url change

--- src/environments/environment.india-prod.ts
+++ src/environments/environment.india-prod.ts
@@ -30,5 +30,5 @@ export const environment = {
   channelIntegration:'https://api.bookonelocal.in/channel-integration',
   // callbackUrl: 'http://localhost:4200/#/details?ReferenceNumber='
   // callbackUrl: 'http://localhost:4200/booking-complete?ReferenceNumber=',
-  callbackUrl: 'https://bookonepms.com/booking-complete?ReferenceNumber=',
+  callbackUrl: 'https://bookone.io/booking-complete?ReferenceNumber=',
 };
